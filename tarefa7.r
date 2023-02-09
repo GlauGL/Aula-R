7c1e7e3ef2cf966411c8c664918d95dc22cd4d93
Glauter Bacellar <124213662+GlauGL@users.noreply.github.com>
2023-02-08 21:30:12 -0300

    Create tarefa7.r

---
+++ tarefa7.r
@@ -0,0 +1,9 @@
+print('adicione a variavlel 1')
+variavel_1<-scan()
+print('adicione a variavel 2')
+variavel_2<-scan()
+
+variavel_3 <-variavel_1
+variavel_1 <-variavel_2
+variavel_2 <-variavel_3
+print(paste(variavel_1,variavel_2))
